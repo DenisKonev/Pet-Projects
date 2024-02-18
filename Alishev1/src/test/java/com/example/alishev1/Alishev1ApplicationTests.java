600107fd8db23d00342f515c9c2a40d64fc0c7c1
Denis Konev <deniskonev1984@gmail.com>
2024-02-18 22:06:59 +0300

    feat: created necessary files to run app

---
+++ Alishev1/src/test/java/com/example/alishev1/Alishev1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alishev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Alishev1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
